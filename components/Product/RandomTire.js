a7a9a9beaca9f1a9168bbb12ad0d65463bcd2988
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-07-29 02:58:13 +0800

    a

--- components/Product/RandomTire.js
+++ components/Product/RandomTire.js
@@ -64,7 +64,7 @@ const RandomTire = () => {
             {tires &&
               tires.map((tire) => (
                 <SwiperSlide key={tire._id} className="tire_slide">
-                  <Link href={`/wheels/${tire.slug}`}>
+                  <Link href={`/tires/${tire.slug}`}>
                     <div className="product-item">
                       <div className="product-item-img">
                         <div className="product-item-set">
