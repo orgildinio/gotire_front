63290285b1f3b14a3cb207c1783787fa99d60b79
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-08-01 00:35:00 +0800

    go

--- app/(page)/layout.js
+++ app/(page)/layout.js
@@ -1,17 +1,17 @@
-import Script from "next/script";
+import NextScript from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <>
-      <Script
+      <NextScript
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-0DFS7DK8C1"
-      ></Script>
-      <Script>
+      ></NextScript>
+      <NextScript>
         window.dataLayer = window.dataLayer || []; function gtag()
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'G-0DFS7DK8C1');
-      </Script>
+      </NextScript>
       {children}
     </>
   );
