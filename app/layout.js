8c9dc190182adbf5c8ee9517cfd89ac519c5bb18
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-08-01 00:38:07 +0800

    go

--- app/layout.js
+++ app/layout.js
@@ -35,9 +35,8 @@ export default function RootLayout({ children }) {
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-0DFS7DK8C1"
       ></Script>
-      <Script>
-        strategy="afterInteractive" window.dataLayer = window.dataLayer || [];
-        function gtag()
+      <Script strategy="afterInteractive">
+        window.dataLayer = window.dataLayer || []; function gtag()
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'G-0DFS7DK8C1');
       </Script>
